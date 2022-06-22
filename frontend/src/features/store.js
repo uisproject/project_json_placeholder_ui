ce38464885879de5a5bd80110ed0654a911903b4
uisproject <uisprojectxyz@gmail.com>
2022-06-22 18:03:52 +0700

    added state management for user auth

--- frontend/src/features/store.js
+++ frontend/src/features/store.js
@@ -2,12 +2,14 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 import { postsAPI } from "../api/api";
+import userAuthReducer from "./userAuthSlice";
 
 const apiMiddleware = [postsAPI.middleware];
 
 const store = configureStore({
   middleware: getDefaultMiddleware().concat([...apiMiddleware]),
   reducer: {
+    userAuth: userAuthReducer,
     // api reducer
     [postsAPI.reducerPath]: postsAPI.reducer,
   },
---
+++ frontend/src/features/userAuthSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const userAuthSlice = createSlice({
+  name: "userAuth",
+  initialState,
+  reducers: {},
+});
+
+export default userAuthSlice.reducer;
