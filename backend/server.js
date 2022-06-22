cad2b04d8cb09bbbc4e2fef33582fedca5be3b74
uisproject <uisprojectxyz@gmail.com>
2022-06-22 12:06:55 +0700

    import data from json placeholder

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,19 @@ const {
   errorMiddleware,
   notFoundMiddleware,
 } = require("./middleware/customErrorMiddleware");
-const { postData } = require("./data/data");
+const { postData, fetchData } = require("./data/data");
 const router = require("./routers/routes");
 
 dotenv.config();
 const server = express();
 
 server.use(express.json());
-server.use(router); // stores all routes
-server.use([notFoundMiddleware, errorMiddleware]); // applying custom middlewares
+
+// import all data from json placeholder, since I don't use any database
+fetchData().then(() => {
+  server.use(router); // stores all routes
+  server.use([notFoundMiddleware, errorMiddleware]); // applying custom middlewares
+});
 
 const PORT = process.env.SERVER_PORT;
 
