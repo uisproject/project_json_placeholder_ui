3ae1a5691113e834bae57ae643c1a9ace619274f
uisproject <uisprojectxyz@gmail.com>
2022-06-22 18:03:34 +0700

    fix button positioning

--- frontend/src/components/Generals/LoginButton.jsx
+++ frontend/src/components/Generals/LoginButton.jsx
@@ -7,9 +7,9 @@ const LoginButton = () => {
     <Button
       block={true}
       type="text"
-      className="inline-flex items-center cursor-pointer p-3"
+      className="inline-flex justify-end items-center cursor-pointer p-3"
     >
-      <LoginOutlined className="text-[2em] " />
+      <LoginOutlined className="text-[1.5em] " />
       <span
         className="font-bold"
         style={{
