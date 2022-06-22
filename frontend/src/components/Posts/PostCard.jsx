93a0632f97b259c063222f77268efe21ce21d9d5
uisproject <uisprojectxyz@gmail.com>
2022-06-22 17:06:33 +0700

    added skeleton

--- frontend/src/components/Posts/PostCard.jsx
+++ frontend/src/components/Posts/PostCard.jsx
@@ -1,21 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import { Card, Skeleton } from "antd";
 
 import { useGetPostsQuery } from "../../api/api";
 import SinglePostCard from "./SinglePostCard";
 
 const PostCard = () => {
   const [page, setPage] = useState(1);
-  const { currentData, isError, isLoading } = useGetPostsQuery({
+  const skeletonItems = useRef(Array.from([1, 2]));
+  let { currentData, isError, isLoading } = useGetPostsQuery({
     page: page,
   });
+  const { Meta } = Card;
 
   return (
     <>
-      {isLoading
-        ? null
-        : currentData.data.map((item) => (
-            <SinglePostCard key={item.postId} {...item} />
-          ))}
+      {isLoading ||
+        currentData.data.map((item) => (
+          <SinglePostCard key={item.postId} {...item} />
+        ))}
+
+      {isLoading &&
+        skeletonItems.current.map((_, idx) => (
+          <Card key={idx} className="my-3 max-w-[1000px] rounded-xl">
+            <Skeleton loading={isLoading} active>
+              <Meta title="Card title" description="This is the description" />
+            </Skeleton>
+          </Card>
+        ))}
     </>
   );
 };
